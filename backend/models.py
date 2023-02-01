30d54e0f329d4ff1a1c283c536d2c0e1f60c1048
Jeff Bailie <jeffrbailie@gmail.com>
2023-02-01 11:09:02 -0800

    Added models.py to handle the creation of the model

---
+++ backend/models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+
+db = SQLAlchemy()
+
+conn_string = 'postgresql://jeffbailie@localhost:5432/mydb'
+
+class Salary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column('CITY', db.String)
+    state = db.Column('STATE', db.String)
+    job = db.Column('JOB TITLE', db.String)
+    salary = db.Column('Annual mean wage', db.Integer)
+
+class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    abbreviation = db.Columb(db.String)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    lattitude = db.Column(db.Double)
+    longitude = db.Column(db.Double)
+    state = db.Column(db.String, ForeignKey("State.name"))
+    
