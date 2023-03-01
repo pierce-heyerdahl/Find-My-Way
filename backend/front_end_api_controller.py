5324408cf4cf093b0cd25b60b1c236cd4f0ac4e7
Jeff Bailie <jeffrbailie@gmail.com>
2023-02-28 19:03:35 -0800

    Implement Job in State and City Searches

--- backend/front_end_api_controller.py
+++ backend/front_end_api_controller.py
@@ -50,6 +50,49 @@ def get_results_from_db_state(user_search):
 
     return(holder)
 
+def get_results_from_db_title_in_state(user_search_job, user_search_state):
+    query = (
+        select(Salary, City)
+        .where((Salary.job == user_search_job) & (Salary.state == user_search_state))
+        .join_from(Salary, City, (Salary.state == City.state) & (Salary.city == City.name))
+        .order_by(Salary.salary.desc())
+        .limit(5)
+    ) 
+
+    results = db.session.execute(query)
+
+    #funciton to tranform result set to object
+    def transform_to_object(row):
+        salary_res = row[0]
+        city_res = row[1]
+        return {"Job Title": salary_res.job, "Salary": salary_res.salary, "City": salary_res.city, "lat": city_res.lat, "lng": city_res.lng, "State": salary_res.state}
+
+    holder = list(map(transform_to_object, results))
+
+    return(holder)
+
+
+def get_results_from_db_city(user_search):
+    query = (
+        select(Salary, City)
+        .where(Salary.city == user_search)
+        .join_from(Salary, City, (Salary.state == City.state) & (Salary.city == City.name))
+        .order_by(Salary.salary.desc())
+        .limit(5)
+    ) 
+
+    results = db.session.execute(query)
+
+    #funciton to tranform result set to object
+    def transform_to_object(row):
+        salary_res = row[0]
+        city_res = row[1]
+        return {"Job Title": salary_res.job, "Salary": salary_res.salary, "City": salary_res.city, "lat": city_res.lat, "lng": city_res.lng, "State": salary_res.state}
+
+    holder = list(map(transform_to_object, results))
+
+    return(holder)
+
 @bp.route("/searchState/<state>", methods=['GET'])
 @cross_origin()
 def search_state(state):
