be84e66278f030c2164d878c9409da541d95bfdc
YJ Kim <yk21ju@gmail.com>
2023-04-10 20:40:19 -0700

    added SalaryRangeSlider

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { TextField, Box, Button, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import backgroundImage from "./images/test2.jpg";
+import RangeSlider from "../components/RangeSlider";
 
 const Main = () => {
   const [jobTitle, setJobTitle] = React.useState("");
   const [state, setState] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [salaryRange, setSalaryRange] = React.useState([200000, 600000]);
   const navigate = useNavigate();
 
   const handleSearch = () => {
-    if (!jobTitle.trim() && !state.trim()) {
-      alert("You must enter either Job Title or State!");
+    if (!jobTitle.trim() && !state.trim() && !city.trim()) {
+      alert("You must enter either Job Title or State or City!");
       return;
     }
 
@@ -26,6 +29,15 @@ const Main = () => {
       searchParams.push("state=" + encodeURI(state.trim()));
     }
 
+    if (city) {
+      searchParams.push("city=" + encodeURI(city.trim()));
+    }
+
+    if (salaryRange) {
+      searchParams.push(`minSalary=${salaryRange[0]}`);
+      searchParams.push(`maxSalary=${salaryRange[1]}`);
+    }
+
     navigate(url + `?${searchParams.join("&")}`);
   };
 
@@ -39,6 +51,18 @@ const Main = () => {
     const newValue = ev.target.value;
 
     setState(newValue);
+  };
+
+  const handleChangeCity = (ev) => {
+    const newValue = ev.target.value;
+
+    setCity(newValue);
+  };
+
+  const handleChangeSalaryRange = (newSalaryRange) => {
+    if (Array.isArray(newSalaryRange) && newSalaryRange.length === 2) {
+      setSalaryRange(newSalaryRange);
+    }
   };
 
   return (
@@ -86,6 +110,31 @@ const Main = () => {
               }
             }}
           />
+          <Typography>OR</Typography>
+          <TextField
+            label="City"
+            type="search"
+            onChange={handleChangeCity}
+            sx={{ backgroundColor: "snow" }}
+            onKeyPress={(ev) => {
+              if (ev.key === "Enter") {
+                ev.preventDefault();
+                handleSearch();
+              }
+            }}
+          />
+          <Stack>
+            <Typography>Salary Range</Typography>
+            <RangeSlider
+              min={5000}
+              onChange={handleChangeSalaryRange}
+              value={salaryRange}
+            />
+            <Typography
+              textAlign="center"
+              width="100%"
+            >{`$${salaryRange[0].toLocaleString()} - $${salaryRange[1].toLocaleString()}`}</Typography>
+          </Stack>
           <Button
             onClick={handleSearch}
             variant="contained"
