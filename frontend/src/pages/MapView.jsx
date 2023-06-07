03e1ee0fa98db0c1ef83571c59762979890313e0
Pierce Heyerdahl <pierce.heyerdahl@bellevuecollege.edu>
2023-06-06 18:45:07 -0700

    Updated MapView and Main for Spring Demo

--- frontend/src/pages/MapView.jsx
+++ frontend/src/pages/MapView.jsx
@@ -36,15 +36,10 @@ const MapView = () => {
 
   let currentPage = parseInt(searchParams.get("page")) || 1;
 
-  const flag = true;
-
-  const randomNumber = Math.round(((Math.random() * 200) * 100) / 100);
-
   const handlePageChange = (event, value) => {
     let newSearchParams = new URLSearchParams(searchParams.toString());
     newSearchParams.set("page", value);
 
-    //window.history.pushState({search: newSearchParams.toString()});
 
     navigate({
       pathname: window.location.pathname,
@@ -54,14 +49,8 @@ const MapView = () => {
     currentPage = value;
   }
 
-  // returns null for the one not searched
-  console.log(jobTitle);
-  console.log(state);
-  console.log(city);
-  console.log(minSalary);
-  console.log(maxSalary);
-
   const [data, setData] = React.useState({results: [], total_pages: 1});
+  const [loading, setLoading] = React.useState(true);
 
   const center = { lat: 47.58536201892643, lng: -122.14791354386401 };
   const positions = [
@@ -71,10 +60,7 @@ const MapView = () => {
   const zoom = 12;
 
   const tilesLoaded = (m) => {
-    console.log(m.getZoom());
-    console.log("tilesloaded");
     var bounds = new window.google.maps.LatLngBounds();
-    console.log(data.results);
     {
       typeof data.results === "undefined"
         ? console.log("No Data Yet")
@@ -82,7 +68,6 @@ const MapView = () => {
             bounds.extend(new window.google.maps.LatLng(row["lat"], row["lng"]))
           );
     }
-    //data.results.map((row) => (bounds.extend(new window.google.maps.LatLng(row["lat"], row["lng"]))));
     m.fitBounds(bounds);
   };
 
@@ -97,53 +82,15 @@ const MapView = () => {
     }
   };
 
-  //const queries = [];
-
-  // searchParams.forEach((val, kv) => {
-  //   queries.push(`${kv}=${val}`);
-  // });
-
-  // const url = `/search/?${queries.join("&")}`;
-
-  // console.log({ url });
-
-  // search/?jobTitle=Lawyer
-  // search/?state=Washington
-  // search/?jobTitle=Lawyer&state=Washington
-
   React.useEffect(() => {
-    console.log("/search/" + jobTitle + "/" + state + "/" + city + "/" + minSalary + "/" + maxSalary + "/" + currentPage)
+    setLoading(true);
     fetch("/search/" + jobTitle + "/" + state + "/" + city + "/" + minSalary + "/" + maxSalary + "/" + currentPage)
       .then((res) => res.json())
       .then((data) => {
         setData({results: data.results, total_pages: data.total_pages});
-        console.log(data);
-        console.log(data[0]);
-        console.log("Making Backend API call");
+        setLoading(false);
       });
   }, [jobTitle, state, city, minSalary, maxSalary, currentPage]);
-
-  // React.useEffect(() => {
-  //   if (state !== null) {
-  //     fetch("/searchState/" + state)
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         setData(data);
-  //         console.log(data);
-  //         console.log(data[0]);
-  //       });
-  //   } else {
-  //     fetch("/searchTitle/" + jobTitle)
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         setData(data);
-  //         console.log(data);
-  //         console.log(data[0]);
-  //       });
-  //   }
-  // }, [jobTitle, state]);
-
-  // http://localhost:3000/mapview?jobTitle=Lawyer&state=Washington
 
   return (
     <Stack
@@ -158,8 +105,35 @@ const MapView = () => {
         overflow: "hidden"
       }}
     >
-      <Box sx={{ width: isMobile ? "auto" : "50%", overflowY: "auto" }}>
-        {jobTitle ? (
+      {!loading ? (
+        <Box sx={{ width: isMobile ? "auto" : "50%", overflowY: "auto" }}>
+          {jobTitle ? (
+            <Typography
+              textAlign="center"
+              sx={{
+                fontSize: "20px",
+                fontWeight: "bold",
+                color: "#F1C40F",
+              }}
+            >
+              Search by Job Title:{" "}
+              <span style={{ color: "#28B463 " }}>{jobTitle.toUpperCase()}</span>
+            </Typography>
+          ) : null}
+          {state ? (
+            <Typography
+              textAlign="center"
+              sx={{
+                fontSize: "20px",
+                fontWeight: "bold",
+                color: "#F1C40F",
+              }}
+            >
+              Search by State:{" "}
+              <span style={{ color: "#28B463 " }}>{state.toUpperCase()}</span>
+            </Typography>
+          ) : null}
+          {city ? (
           <Typography
             textAlign="center"
             sx={{
@@ -168,23 +142,86 @@ const MapView = () => {
               color: "#F1C40F",
             }}
           >
-            Search by Job Title:{" "}
-            <span style={{ color: "#28B463 " }}>{jobTitle.toUpperCase()}</span>
+            Search by City:{" "}
+            <span style={{ color: "#28B463 " }}>{city.toUpperCase()}</span>
           </Typography>
-        ) : null}
-        {state ? (
-          <Typography
-            textAlign="center"
-            sx={{
-              fontSize: "20px",
-              fontWeight: "bold",
-              color: "#F1C40F",
-            }}
-          >
-            Search by State:{" "}
-            <span style={{ color: "#28B463 " }}>{state.toUpperCase()}</span>
-          </Typography>
-        ) : null}
+          ) : null}
+          {!loading ? (
+            <Box
+              sx={{
+                textAlign: "left",
+                margin: "auto",
+                display: "flex",
+                flexDirection: isMobile ? "column-reverse" : "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "fit-content",
+              }}
+            >
+              {typeof data.results === "undefined" ? (
+                <p>Loading...</p>
+              ) : (
+                <TableContainer component={Paper} sx={{ maxWidth: "100%", margin: "1em" }}>
+                <Table aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>No.</TableCell>
+                      <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>Location</TableCell>
+                      <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>Job Title</TableCell>
+                      <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>Salary</TableCell>
+                      <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>Job Links</TableCell>
+                      <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>Cost of Living</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {data.results.map((row, i) => (
+                      <TableRow
+                        key={i}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>{((currentPage - 1) * RESULTS_PER_PAGE) + i + 1}</TableCell>
+                        <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>{row["City"] + ", " + row["State"]}</TableCell>
+                        <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>{row["Job Title"]}</TableCell>
+                        <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>${row["Salary"].toLocaleString()}</TableCell>
+                        <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default' }}>
+                          <a href={encodeURI(`https://www.google.com/search?q=${row["City"]}+${row["State"]}+${row["Job Title"]}&ibp=htl;jobs`)}
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                          >
+                            Search on Google
+                          </a>
+                        </TableCell>
+                        <TableCell align="left" sx={{ p: isMobileSmall ? '0.15em' : isMobileLarge ? '0.5em' : 'default', color: row["coli"] > 100 ? 'red' : 'green' }}>
+                          {row["coli"] > 100 ? '+' : '-'}{(row["coli"] - 100).toFixed(1)}%
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            )}
+
+
+              <Pagination count={data.total_pages} page={currentPage} onChange={handlePageChange} sx={{padding: "0.25em"}} />
+            </Box>
+          ) : null}
+
+          <Divider orientation="horizontal" flexItem sx={{ margin: "2em 0" }} />
+          {/*where graph will go*/}
+          <Box sx={{ display: "flex", justifyContent: "center", margin: "1em" }}>
+            <Box
+              sx={{
+                maxWidth: "100%",
+                maxHeight: "100%",
+                width: "600px",
+                height: "300px",
+              }}
+            >
+              <BarChart chartData={convertBarChartData(data)} />
+            </Box>
+          </Box>
+        </Box>
+      ) : (
         <Box
           sx={{
             textAlign: "left",
@@ -196,66 +233,9 @@ const MapView = () => {
             width: "fit-content",
           }}
         >
-          {typeof data.results === "undefined" ? (
-            <p>Loading...</p>
-          ) : (
-            <TableContainer component={Paper} sx={{ maxWidth: "100%", margin: "1em" }}>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>No.</TableCell>
-                  <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>Location</TableCell>
-                  <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>Job Title</TableCell>
-                  <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>Salary</TableCell>
-                  <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>Job Links</TableCell>
-                  <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>Cost of Living</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.results.map((row, i) => (
-                  <TableRow
-                    key={i}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>{((currentPage - 1) * RESULTS_PER_PAGE) + i + 1}</TableCell>
-                    <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>{row["City"] + ", " + row["State"]}</TableCell>
-                    <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>{row["Job Title"]}</TableCell>
-                    <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>${row["Salary"].toLocaleString()}</TableCell>
-                    <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default' }}>
-                      <a href={encodeURI(`https://www.google.com/search?q=${row["City"]}+${row["State"]}+${row["Job Title"]}&ibp=htl;jobs`)}
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                      >
-                        Search on Google
-                      </a>
-                    </TableCell>
-                    <TableCell align="left" sx={{ p: isMobileSmall ? 0 : isMobileLarge ? '0.5em' : 'default', color: row["coli"] > 100 ? 'red' : 'green' }}>{row["coli"]}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
-
-
-          <Pagination count={data.total_pages} page={currentPage} onChange={handlePageChange} sx={{padding: "0.25em"}} />
+          <Typography>Loading...</Typography>
         </Box>
-
-        <Divider orientation="horizontal" flexItem sx={{ margin: "2em 0" }} />
-        {/*where graph will go*/}
-        <Box sx={{ display: "flex", justifyContent: "center", margin: "1em" }}>
-          <Box
-            sx={{
-              maxWidth: "100%",
-              maxHeight: "100%",
-              width: "600px",
-              height: "300px",
-            }}
-          >
-            <BarChart chartData={convertBarChartData(data)} />
-          </Box>
-        </Box>
-      </Box>
+      )}
 
       <Box
         border="1px grey solid"
